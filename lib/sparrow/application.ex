3e03bd6fbb20103d974ea3c07b516e7ce20e18bd
Yuri Artemev <i@artemeff.com>
2019-03-25 13:32:47 +0300

    init

---
+++ lib/sparrow/application.ex
@@ -0,0 +1,20 @@
+defmodule Sparrow.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Sparrow.Worker.start_link(arg)
+      # {Sparrow.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Sparrow.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
