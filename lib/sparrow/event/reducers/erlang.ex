0f56b06530be72c5f0a86e1cad8826b1d251ce43
Yuri Artemev <i@artemeff.com>
2019-05-14 12:54:58 +0300

    use put_error in erlang reducer

--- lib/sparrow/event/reducers/erlang.ex
+++ lib/sparrow/event/reducers/erlang.ex
@@ -3,21 +3,20 @@ defmodule Sparrow.Event.Reducers.Erlang do
   @behaviour Sparrow.Event.Reducer
 
   def reduce(%{msg: {~c'Error in process ~p with exit value:~n~p~n', [pid, reason]}}, event) do
-    proc_crash(event, pid, Sparrow.format_reason(reason))
+    proc_crash(event, pid, reason)
   end
 
   def reduce(%{msg: {~c'Error in process ~p on node ~p with exit value:~n~p~n', [pid, _node, reason]}}, event) do
-    proc_crash(event, pid, Sparrow.format_reason(reason))
+    proc_crash(event, pid, reason)
   end
 
   def reduce(_, event) do
     event
   end
 
-  defp proc_crash(event, pid, {reason, stacktrace}) do
+  defp proc_crash(event, pid, reason) do
     event
-    |> Sparrow.Event.put_exception(:error, reason, stacktrace)
-    |> Sparrow.Event.put_stacktrace(stacktrace)
+    |> Sparrow.Event.put_error(reason)
     |> Sparrow.Event.put_extra(%{pid: pid})
   end
 end
